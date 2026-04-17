10d310d5a14d0452e5f932ee35ca63391afd625f
Shaun Geaney <sa.geaney@gmail.com>
2026-04-17 13:06:35 +0800

    Created contructor for MonteCarloSimulator class.

--- src/montecarlo/simulate.py
+++ src/montecarlo/simulate.py
@@ -2,6 +2,7 @@
 # Date: 17-Apr-2026
 
 import numpy as np
+import numpy.random as npr
 
 from dataclasses import dataclass
 
@@ -22,4 +23,15 @@ class EuropeanOption:
     def d2(self):
         return self.d1 - self.sigma * np.sqrt(self.T)
     
-    
+
+@dataclass
+class MonteCarloSimulator:
+
+    option: EuropeanOption  # Option parameters
+    num_t_steps: int = 250  # Avg number of trading days in a year
+    num_paths: int = 1_000  # Number of Monte Carlo paths to simulate
+
+    def __post_init__(self):
+        self.dt: np.ndarray = self.option.T / self.num_t_steps
+        self.t: np.ndarray = np.linspace(0, self.option.T, self.num_t_steps + 1)
+        self.results: np.ndarray = np.empty((self.num_paths, self.num_t_steps + 1), dtype=np.float64)
